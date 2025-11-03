ce613bc44e3b1fa1ae3645094d7e2c3d7f1e10e7
dentiny <dentinyhao@gmail.com>
2025-11-03 22:43:31 +0000

    alter table sql

--- src/client/motherduck_table_catalog_entry.cpp
+++ src/client/motherduck_table_catalog_entry.cpp
@@ -16,73 +16,9 @@
 #include "logical_remote_alter_table.hpp"
 #include "motherduck_catalog.hpp"
 #include "motherduck_schema_catalog_entry.hpp"
+#include "utils/catalog_utils.hpp"
 
 namespace duckdb {
-
-namespace {
-// Helper function to generate ALTER TABLE SQL (duplicated from motherduck_schema_catalog_entry.cpp)
-string GenerateAlterTableSQL(AlterTableInfo &info, const string &table_name) {
-	string sql = "ALTER TABLE " + table_name + " ";
-	
-	switch (info.alter_table_type) {
-	case AlterTableType::ADD_COLUMN: {
-		auto &add_info = info.Cast<AddColumnInfo>();
-		sql += "ADD COLUMN ";
-		if (add_info.if_column_not_exists) {
-			sql += "IF NOT EXISTS ";
-		}
-		sql += add_info.new_column.Name() + " " + add_info.new_column.Type().ToString();
-		if (add_info.new_column.HasDefaultValue()) {
-			sql += " DEFAULT " + add_info.new_column.DefaultValue().ToString();
-		}
-		break;
-	}
-	case AlterTableType::REMOVE_COLUMN: {
-		auto &remove_info = info.Cast<RemoveColumnInfo>();
-		sql += "DROP COLUMN ";
-		if (remove_info.if_column_exists) {
-			sql += "IF EXISTS ";
-		}
-		sql += remove_info.removed_column;
-		break;
-	}
-	case AlterTableType::RENAME_COLUMN: {
-		auto &rename_info = info.Cast<RenameColumnInfo>();
-		sql += "RENAME COLUMN " + rename_info.old_name + " TO " + rename_info.new_name;
-		break;
-	}
-	case AlterTableType::RENAME_TABLE: {
-		auto &rename_info = info.Cast<RenameTableInfo>();
-		sql = "ALTER TABLE " + table_name + " RENAME TO " + rename_info.new_table_name;
-		break;
-	}
-	case AlterTableType::ALTER_COLUMN_TYPE: {
-		auto &change_info = info.Cast<ChangeColumnTypeInfo>();
-		sql += "ALTER COLUMN " + change_info.column_name + " TYPE " + change_info.target_type.ToString();
-		break;
-	}
-	case AlterTableType::SET_DEFAULT: {
-		auto &set_default_info = info.Cast<SetDefaultInfo>();
-		sql += "ALTER COLUMN " + set_default_info.column_name + " SET DEFAULT " + set_default_info.expression->ToString();
-		break;
-	}
-	case AlterTableType::SET_NOT_NULL: {
-		auto &set_not_null_info = info.Cast<SetNotNullInfo>();
-		sql += "ALTER COLUMN " + set_not_null_info.column_name + " SET NOT NULL";
-		break;
-	}
-	case AlterTableType::DROP_NOT_NULL: {
-		auto &drop_not_null_info = info.Cast<DropNotNullInfo>();
-		sql += "ALTER COLUMN " + drop_not_null_info.column_name + " DROP NOT NULL";
-		break;
-	}
-	default:
-		throw NotImplementedException("Unsupported ALTER TABLE type for remote execution");
-	}
-	
-	return sql;
-}
-} // namespace
 
 MotherduckTableCatalogEntry::MotherduckTableCatalogEntry(Catalog &motherduck_catalog_p, DatabaseInstance &db_instance_p,
                                                          DuckTableEntry *duck_table_entry_p,
@@ -96,16 +32,14 @@ MotherduckTableCatalogEntry::MotherduckTableCatalogEntry(Catalog &motherduck_cat
 unique_ptr<CatalogEntry> MotherduckTableCatalogEntry::AlterEntry(ClientContext &context, AlterInfo &info) {
 	DUCKDB_LOG_DEBUG(db_instance, "MotherduckTableCatalogEntry::AlterEntry");
 	
-	// AlterEntry is not called for remote tables - ALTER operations are intercepted
-	// at the schema level in MotherduckSchemaCatalogEntry::Alter()
+	// AlterEntry is not called for remote tables - ALTER operations are intercepted at the schema level in MotherduckSchemaCatalogEntry::Alter.
 	return duck_table_entry->AlterEntry(context, info);
 }
 
 unique_ptr<CatalogEntry> MotherduckTableCatalogEntry::AlterEntry(CatalogTransaction transaction, AlterInfo &info) {
 	DUCKDB_LOG_DEBUG(db_instance, "MotherduckTableCatalogEntry::AlterEntry (CatalogTransaction)");
 	
-	// AlterEntry is not called for remote tables - ALTER operations are intercepted
-	// at the schema level in MotherduckSchemaCatalogEntry::Alter()
+	// AlterEntry is not called for remote tables - ALTER operations are intercepted at the schema level in MotherduckSchemaCatalogEntry::Alter.
 	return duck_table_entry->AlterEntry(std::move(transaction), info);
 }
 
@@ -230,15 +164,13 @@ void MotherduckTableCatalogEntry::BindUpdateConstraints(Binder &binder, LogicalG
 
 virtual_column_map_t MotherduckTableCatalogEntry::GetVirtualColumns() const {
 	DUCKDB_LOG_DEBUG(db_instance, "MotherduckTableCatalogEntry::GetVirtualColumns");
-	// Don't use duck_table_entry as it may be stale after ALTER TABLE
-	// Use the parent class implementation instead
+	// Don't use duck_table_entry as it may be stale after ALTER TABLE Use the parent class implementation instead.
 	return DuckTableEntry::GetVirtualColumns();
 }
 
 vector<column_t> MotherduckTableCatalogEntry::GetRowIdColumns() const {
 	DUCKDB_LOG_DEBUG(db_instance, "MotherduckTableCatalogEntry::GetRowIdColumns");
-	// Don't use duck_table_entry as it may be stale after ALTER TABLE
-	// Use the parent class implementation instead
+	// Don't use duck_table_entry as it may be stale after ALTER TABLE Use the parent class implementation instead.
 	return DuckTableEntry::GetRowIdColumns();
 }
 
--- src/include/utils/catalog_utils.hpp
+++ src/include/utils/catalog_utils.hpp
@@ -4,7 +4,13 @@
 
 namespace duckdb {
 
+// Forward declaration.
+struct AlterTableInfo;
+
 // Util function to sanitize query and remove all occurrences of the catalog prefix from SQL string.
 string SanitizeQuery(const string &sql, const string &catalog_name);
+
+// Generate SQL statement to alter table.
+string GenerateAlterTableSQL(AlterTableInfo &info, const string &table_name);
 
 }  // namespace duckdb
